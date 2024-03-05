2b40b58dcdc5626ff8ff32556c89797139c570ac
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-03-04 22:51:37 -0500

    console printing and file

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
-const message: string = "Hello world!";
+import { mkdirSync, writeFileSync } from "fs";
 
-console.log(message);
+let outputMessage = "";
+const base = 5;
+const headerMessage = `
+==================================
+       Table of the ${base}
+==================================\n
+`;
+
+for (let i = 0; i <= 10; i++) {
+	outputMessage += `${base} x ${i} = ${base * i}\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+const outputPath = `outputs`;
+mkdirSync(outputPath, { recursive: true });
+writeFileSync(`outputs/table-${base}.txt`, outputMessage);
