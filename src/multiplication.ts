ab59c6095cea0371401de71634a83b4617d5b978
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-03-05 19:26:13 -0500

    yarg in multiplication

--- src/multiplication.ts
+++ src/multiplication.ts
@@ -1,20 +1,24 @@
 import { mkdirSync, writeFileSync } from "fs";
+import { yarg } from "./config/plugings/yargs.plugin";
+
+// console.log(yarg);
+const { b, l: limit, s: showTable } = yarg;
 
 let outputMessage = "";
-const base = 5;
+const base = b ?? 5;
 const headerMessage = `
 ==================================
        Table of the ${base}
 ==================================\n
 `;
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i <= limit; i++) {
 	outputMessage += `${base} x ${i} = ${base * i}\n`;
 }
 
 outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-
+if (showTable) console.log(outputMessage);
+console.log("File created");
 const outputPath = `outputs`;
 mkdirSync(outputPath, { recursive: true });
 writeFileSync(`outputs/table-${base}.txt`, outputMessage);
