2ddfc0730d73605df4d03408759e4673e34aa1b8
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-07-25 11:06:59 -0500

    server-app test

--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -2,22 +2,21 @@ import { CreateTable } from "../domain/use-cases/create-table.use-case";
 import { SaveFile } from "../domain/use-cases/save-file.use.case";
 import { ServerApp } from "./server-app";
 describe("Test Server app", () => {
-	
-  const options = {
+	const options = {
 		base: 2,
 		limit: 10,
 		showTable: false,
 		fileDestination: "test-outputs",
 		fileName: "test-filename",
-  };
+	};
 
-  test("Should create ServerApp instance", () => {
+	test("Should create ServerApp instance", () => {
 		const serverApp = new ServerApp();
 		expect(serverApp).toBeInstanceOf(ServerApp);
 		expect(typeof ServerApp.run).toBe("function");
-  });
+	});
 
-  test("should run ServerApp with options", () => {
+	/* test("should run ServerApp with options", () => {
 		const logSpy = jest.spyOn(console, "log");
 		const createTableSpy = jest.spyOn(CreateTable.prototype, "execute");
 		const saveFileSpy = jest.spyOn(SaveFile.prototype, "execute");
@@ -41,5 +40,33 @@ describe("Test Server app", () => {
 			fileDestination: options.fileDestination,
 			fileName: options.fileName,
 		});
-  });
+  }); */
+
+	test("Should run with custom values mocked", () => {
+		const logMock = jest.fn();
+		const logErrorMock = jest.fn();
+		const createMock = jest.fn().mockReturnValue("1 x 2 = 2");
+		const saveFileMock = jest.fn().mockReturnValue(true);
+
+		console.log = logMock;
+		console.error = logErrorMock;
+		CreateTable.prototype.execute = createMock;
+		SaveFile.prototype.execute = saveFileMock;
+
+		ServerApp.run(options);
+
+		expect(logMock).toHaveBeenCalledWith("Server running...");
+		expect(createMock).toHaveBeenCalledWith({
+			base: options.base,
+			limit: options.limit,
+		});
+		expect(saveFileMock).toHaveBeenCalledWith({
+			fileContent: "1 x 2 = 2",
+			fileDestination: options.fileDestination,
+			fileName: options.fileName,
+			base: options.base,
+		});
+
+		expect(logMock).toHaveBeenCalledWith("File created! 😀");
+	});
 });
